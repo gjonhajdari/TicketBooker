c543895b3d72c3818e2d1003477fec0868d8bc9e
getuarkelmendi <102876616+getuarkelmendi@users.noreply.github.com>
2023-01-05 17:33:09 +0100

    input password hide-show

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,35 @@ $(document).ready(() => {
 	})
 
 });
+
+/*
+-------------------------------------------------
+Password hide-show ------------------------------------
+-------------------------------------------------
+*/
+function togglePw(){
+	var password = document.querySelector('[name=password]');
+
+	if(password.getAttribute('type')==='password'){
+		password.setAttribute('type', 'text');
+		document.getElementById("font").style.color='green';
+	}
+	else{
+		password.setAttribute('type', 'password');
+		document.getElementById("font").style.color='red';
+	}
+}
+
+
+function togglepw(){
+	var password = document.querySelector('[name=password]');
+
+	if(password.getAttribute('type')==='password'){
+		password.setAttribute('type', 'text');
+		document.getElementById("font").style.color='green';
+	}
+	else{
+		password.setAttribute('type', 'password');
+		document.getElementById("font").style.color='red';
+	}
+}
