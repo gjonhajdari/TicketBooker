7ddb1cfbf863090feda7a7a309bd2fafe52e6fc9
getuarkelmendi <102876616+getuarkelmendi@users.noreply.github.com>
2023-01-19 21:23:04 +0100

    javascript

--- src/js/app.js
+++ src/js/app.js
@@ -57,3 +57,12 @@ function togglepw(){
 function error(){
 	alert("Can't create an account because there is no database")
 }
+
+		let answer1 = "It's alright";
+		let answer2 = "He is called 'Johnny'";
+		let answer3 = 'He is called "Johnny"';
+
+		document.getElementById("demo").innerHTML =
+		answer1 + "<br>" + 
+		answer2 + "<br>" + 
+		answer3;
