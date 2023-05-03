e2d6d81ea180670961a8badc9d049b9db1cbccec
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-05-03 21:33:54 +0200

    Fixed media paths

---
+++ src/templates/footer.php
@@ -0,0 +1,20 @@
+<!-- Footer -->
+<footer>
+	<p id="copyright">Copyright &copy; 2023 TicketBooker. All rights reserved</p>
+	<div class="icons">
+		<a href="https://www.instagram.com" target="_blank">
+			<img src="assets/icons/Instagram.svg" alt="Instagram" class="icon">
+		</a>
+		<a href="https://www.twitter.com" target="_blank">
+			<img src="assets/icons/Twitter.svg" alt="Twitter" class="icon">
+		</a>
+		<a href="https://www.linkedin.com" target="_blank">
+			<img src="assets/icons/Linkedin.svg" alt="Linkedin" class="icon">
+		</a>
+	</div>
+	<div class="links">
+		<a href="assets/extra/PRIVACY_POLICY.pdf" target="_blank" class="footer-link">Privacy Policy</a>
+		<a href="assets/extra/TERMS_AND_CONDITIONS.pdf" target="_blank" class="footer-link">Terms of Use</a>
+		<a href="allfeatures.html" target="_blank" class="footer-link">All features</a>
+	</div>
+</footer>
