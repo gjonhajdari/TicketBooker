1823e545d9d3ab21cfcc623a57923e401aa4f9c5
Vera Llugiqi <115923848+VeraLlugiqi@users.noreply.github.com>
2023-05-06 23:08:25 +0200

    Add files via upload

--- public/login.php
+++ public/login.php
@@ -9,12 +9,38 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel='icon' type='image/x-icon' href='assets/icons/Favicon.svg'>
-	<link rel='stylesheet' href='css/palette-dark.css'>
+	<?php
+		if ($isDark == true) {
+			echo "<link rel='stylesheet' href='css/palette-dark.css'>";
+		} else {
+			echo "<link rel='stylesheet' href='css/palette-light.css'>";
+		}
+	?>
 	<link rel="stylesheet" href="css/general.css">
 	<link rel="stylesheet" href="css/login.css">
+	
 	<script src="https://kit.fontawesome.com/26e97bbe8d.js" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 	<script src="js/app.js"></script>
+	<style>
+		
+.alert {
+	padding: 0.75rem 1.25rem;
+	margin-bottom: 1rem;
+	border: 1px solid transparent;
+	border-radius: 0.25rem;
+	width: 100%;
+	margin-left: auto;
+	margin-right: auto;
+  }
+  .alert-danger {
+	color: #721c24;
+	background-color:#f0afaf;
+	border-color: #f5c6cb;
+  }
+  
+  
+	</style>
 </head>
 
 <body>
@@ -30,23 +56,54 @@
 				<h1>Welcome back!</h1>
 				<p>
 					Don't have an account?
-					<a href="signup.php">Sign up</a>.
+					<a href="signup.html">Sign up</a>.
 				</h2>
 			</div>
+			<?php
+
+require_once('../src/modules/db.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
+	$conn or die("Connection failed: ".mysqli_connect_error());
 	
-			<form action="../src/modules/loginConnection.php" method="POST">
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+
+		$sql = "SELECT * FROM `user` WHERE email = '$email'";
+		$result = mysqli_query($conn, $sql);
+
+		$user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+		if ($user) {
+			if (password_verify($password, $user["password"])) {
+				session_start();
+				$_SESSION["user"] = "yes";
+				header('location:../../public/index.php');
+				die();
+			}
+		}else{
+			echo "<div class='alert alert-danger '>Email or Password do not match</div>";
+		}
+	
+};
+
+mysqli_close($conn);
+
+?>
+			<!-- // ../src/modules/loginConnection.php	 -->
+			<form action="login.php" method="POST" class="<?php echo $isDark ? '' : 'border-light'; ?>">
 				<input type="email" name="email" required="required" placeholder="Email address" class="input">
 	
-				<div class="password">
+				<div id="password">
 					<input type="password" name="password" required="required" placeholder="Password" class="input" id="passwordInput">
-					<i class="fa-solid fa-eye-slash toggle-visibility" id="toggle-visibility"></i>
+					<i class="fa-solid fa-eye-slash" id="toggle-visibility"></i>
+					<!-- <span><i class="fa-solid fa-eye" id="eye" onclick="myfunction()"></i></span>  -->
 				</div>
 			
 				<div id="checkbox">
 					<input type="checkbox" name="checkbox" id="check">
 					<p>Remember me</p>
 				</div>
-	          <input type="submit" name="submit" id="button"  class="btn">
+	          <input type="submit" name="submit" id="button"  class="btn" value="Login">
 			</form>
 		</div>
 
--- public/signup.php
+++ public/signup.php
@@ -1,5 +1,13 @@
 <?php $isDark = false; ?>
 
+<?php
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: index.php");
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,12 +29,35 @@
 	<script src="https://kit.fontawesome.com/26e97bbe8d.js" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 	<script src="js/app.js"></script>
-</head>
 
+	<style>
+	.alert {
+  padding: 0.75rem 1.25rem;
+  margin-bottom: 1rem;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  width: 50%;
+  
+  margin-right: auto;
+}
+
+.alert-success {
+  color: #155724;
+  background-color: #d4edda;
+  border-color: #c3e6cb;
+}
+
+.alert-danger {
+  color: #721c24;
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+}
+</style>
 <body>
 
 	<!-- Navigation Bar -->
 	<?php include "../src/templates/navbar.php"; ?>
+
 
 	<!-- Main content -->
 	<main>
@@ -34,14 +65,81 @@
 		<h1>Create an account</h1> <br>
 		<h6>Already have one? <a href="login.html" id="a">Log in</a></h6>
 		<br>
-		<form action="../src/modules/signupConnection.php" method="POST" class="<?php echo $isDark ? '' : 'border-light'; ?>">
+
+
+		<?php 
+require_once('../src/modules/db.php');
+if ($_SERVER['REQUEST_METHOD'] == 'POST'  &&  isset($_POST['submit'])) {
+	$conn or die("Connection failed: ".mysqli_connect_error());
+		
+		$name_buss = $_POST['name_buss'];
+		$email = $_POST['email'];
+		$email_confirm = $_POST['email_confirm'];
+		$password = $_POST['password'];
+		$password_confirm = $_POST['password_confirm'];
+
+		$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+		
+		$errors = array();
+           
+		 if (empty($_POST['user_adm']) || empty($_POST['name_buss']) || empty($_POST['email']) || empty($_POST['email_confirm']) || empty($_POST['password']) || empty($_POST['password_confirm']) || empty($_POST['checkbox']) ) {
+			
+			//echo "<div class='alert alert-danger w-50 p-3'>All fields are required</div>";
+             array_push($errors,"All fields are required");
+           }
+           if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            array_push($errors, "Email is not valid");
+           }
+           if (strlen($password)<8) {
+            array_push($errors,"Password must be at least 8 charactes long");
+           }
+		   if ($email!==$email_confirm) {
+            array_push($errors,"Password does not match");
+           }
+           if ($password!==$password_confirm) {
+            array_push($errors,"Password does not match");
+           }
+           require_once "../src/modules/db.php";
+           $sql = "SELECT * FROM `user` WHERE email = '$email'";
+           $result = mysqli_query($conn, $sql);
+           $rowCount = mysqli_num_rows($result);
+           if ($rowCount>0) {
+            array_push($errors,"Email already exists!");
+           }
+           if (count($errors)>0) {
+            foreach ($errors as  $error) {
+                echo "<div class='alert alert-danger w-50 p-3'>$error</div>";
+            }
+           }else{
+
+            $user_adm = $_POST['user_adm'];
+			$checkbox = $_POST['checkbox'];
+            $sql = "INSERT INTO user (user_adm, name_buss, email, password, checkbox) VALUES ( ?, ?,?, ? ,?)";
+            $statemant = mysqli_stmt_init($conn);
+            $prepareStatemant = mysqli_stmt_prepare($statemant,$sql);
+            if ($prepareStatemant) {
+                mysqli_stmt_bind_param($statemant,"sssss", $user_adm,$name_buss, $email, $hashed_password, $checkbox);
+                mysqli_stmt_execute($statemant);
+                echo "<div class='alert alert-success w-50 p-3'>You are registered successfully.</div>";
+            }else{
+                die("Something went wrong");
+            }
+           }
+	}	  
+ mysqli_close($conn);
+?>
+
+
+
+		<!-- ../src/modules/signupConnection.php -->
+		<form action="" method="POST" class="<?php echo $isDark ? '' : 'border-light'; ?>">
 		<div>
 		<input type="radio" name="user_adm" value="ADMINISTRATOR"/>Administrator</td> 
 		<input type="radio" name="user_adm" value="USER"/>User</td> 
 		</div>	
 		
 		<div>
-				<input name="name_bus" id="email" type="text" placeholder="First name or Business name" />
+				<input name="name_buss" id="email" type="text" placeholder="First name or Business name" />
 			</div>
 
 			<br>
@@ -72,7 +170,7 @@
 			<div class="check">
 
 				<p id="agree">
-					<input type="checkbox" name="checkbox" id="checkbox"> I agree to the
+					<input type="checkbox" name="checkbox" id="checkbox" value="AGREE"> I agree to the
 					<a href="../assets/PRIVACY_POLICY.pdf" target="_blank" class="footer-linkss">Privacy Policy</a>&
 					<a href="../assets/TERMS_AND_CONDITIONS.pdf" target="_blank" class="footer-links">Terms of Use</a>
 				</p>
