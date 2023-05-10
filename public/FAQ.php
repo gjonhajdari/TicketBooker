bd9f14bfdc6349b485edcc00c4dd0bc42bde9dc4
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2023-05-10 13:31:24 +0200

    Added the Bootstrap

--- public/FAQ.php
+++ public/FAQ.php
@@ -9,13 +9,29 @@ $full_name = 'Gjon Hajdari';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="Bootstrap" href="Bootstrap">
     <title>FAQ</title>
+    <?php
+    if ($isDark == true) {
+        echo "<link rel='stylesheet' href='css/palette-dark.css'>";
+    } else {
+        echo "<link rel='stylesheet' href='css/palette-light.css'>";
+    }
+    ?>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/general.css">
+    <link rel="stylesheet" href="css/createTicket.css">
+    <script src="https://kit.fontawesome.com/26e97bbe8d.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js"
+        integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script src="js/app.js"></script>
 </head>
+
 <body>
     <!-- <form action="../src/modules/FAQConnection.php" method="post">
         <input type="text" name="username_or_id" id="username_or_id" placeholder="Username or ID">
@@ -32,33 +48,33 @@ $full_name = 'Gjon Hajdari';
   
   
   
-  <?php 
+  <?php
+
+  //   require_once('../src/modules/db.php');
   
-//   require_once('../src/modules/db.php');
-
-//   $sql = "SELECT * FROM questions";
+  //   $sql = "SELECT * FROM questions";
 // $result = mysqli_query($conn, $sql);
-
-// // Loop through the results and display each question and answer
+  
+  // // Loop through the results and display each question and answer
 // if (mysqli_num_rows($result) > 0) {
 //     while ($row = mysqli_fetch_assoc($result)) {
 //         echo "<div class='question'>";
 //         echo "<h3>" . $row['question'] . "</h3>";
-        
-//         // Display the answer if it exists
+  
+  //         // Display the answer if it exists
 //         if ($row['answer']) {
 //             echo "<div class='answer'>";
 //             echo "<p>" . $row['answer'] . "</p>";
 //             echo "</div>";
 //         }
-        
-//         echo "</div>";
+  
+  //         echo "</div>";
 //     }
 // } else {
 //     echo "No questions found.";
 // } 
-
-//     Close the statement
+  
+  //     Close the statement
 //     mysqli_stmt_close($stmt);
 //     mysqli_close($conn);
 //     Show a success message to the user
@@ -67,4 +83,5 @@ $full_name = 'Gjon Hajdari';
   ?> 
   -->
 </body>
+
 </html>
