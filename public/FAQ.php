d6bc321069daf7766460e49f589bca99a35de2fa
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2023-05-10 13:30:17 +0200

    Added bootstrap

--- public/FAQ.php
+++ public/FAQ.php
@@ -1,13 +1,23 @@
+<?php
+
+$isDark = true;
+$isLoggedIn = true;
+$avatar = 10;
+$full_name = 'Gjon Hajdari';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="Bootstrap" href="Bootstrap">
     <title>FAQ</title>
 </head>
 <body>
-    <form action="../src/modules/FAQConnection.php" method="post">
+    <!-- <form action="../src/modules/FAQConnection.php" method="post">
         <input type="text" name="username_or_id" id="username_or_id" placeholder="Username or ID">
         <br>
         <input type="text" name="question" id="question" placeholder="Write your question">
@@ -17,41 +27,44 @@
 
 
 
-  <?php
   
-  require_once('../src/modules/db.php');
-
-  $sql = "SELECT * FROM questions";
-$result = mysqli_query($conn, $sql);
-
-// Loop through the results and display each question and answer
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<div class='question'>";
-        echo "<h3>" . $row['question'] . "</h3>";
-        
-        // Display the answer if it exists
-        if ($row['answer']) {
-            echo "<div class='answer'>";
-            echo "<p>" . $row['answer'] . "</p>";
-            echo "</div>";
-        }
-        
-        echo "</div>";
-    }
-} else {
-    echo "No questions found.";
-}
-
-    // Close the statement
-    // mysqli_stmt_close($stmt);
-    mysqli_close($conn);
-    // Show a success message to the user
-    // echo "Your question has been submitted. Thank you!";
-
-
-
   
-  ?>
+  
+  
+  
+  <?php 
+  
+//   require_once('../src/modules/db.php');
+
+//   $sql = "SELECT * FROM questions";
+// $result = mysqli_query($conn, $sql);
+
+// // Loop through the results and display each question and answer
+// if (mysqli_num_rows($result) > 0) {
+//     while ($row = mysqli_fetch_assoc($result)) {
+//         echo "<div class='question'>";
+//         echo "<h3>" . $row['question'] . "</h3>";
+        
+//         // Display the answer if it exists
+//         if ($row['answer']) {
+//             echo "<div class='answer'>";
+//             echo "<p>" . $row['answer'] . "</p>";
+//             echo "</div>";
+//         }
+        
+//         echo "</div>";
+//     }
+// } else {
+//     echo "No questions found.";
+// } 
+
+//     Close the statement
+//     mysqli_stmt_close($stmt);
+//     mysqli_close($conn);
+//     Show a success message to the user
+//     echo "Your question has been submitted. Thank you!";
+  
+  ?> 
+  -->
 </body>
 </html>
