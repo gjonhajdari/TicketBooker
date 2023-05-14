97952ee3cabc8e1516261d47105c5d842c5b4d95
ZanaMisini <118012775+ZanaMisini@users.noreply.github.com>
2023-05-14 23:54:18 +0200

    Update navbarLoggedin.php

--- src/templates/navbarLoggedin.php
+++ src/templates/navbarLoggedin.php
@@ -1,4 +1,17 @@
 <!-- Navigation bar -->
+<?php
+
+// require('signup.php');
+// if(!empty($_SESSION['id'])){
+// 	$id = $_SESSION["id"];
+// 	$result = mysqli_query($conn, "SELECT * FROM  users WHERE id = $id");
+// 	$row = mysqli_fetch_assoc($result);
+
+// }else{
+// 	header("Location: login.php");
+// }
+
+?>
 <nav 
 	class="navbar <?php
 						echo $isDark ? '' : ' navbar-light';
