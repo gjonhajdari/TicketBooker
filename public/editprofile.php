af558235e26977e5b58304bfbd7a59fd2e783f42
ZanaMisini <118012775+ZanaMisini@users.noreply.github.com>
2023-05-14 23:49:35 +0200

    session editProfile.php

--- public/editprofile.php
+++ public/editprofile.php
@@ -1,5 +1,17 @@
 <?php
 
+
+// require('signup.php');
+// if(!empty($_SESSION['id'])){
+// 	$id = $_SESSION["id"];
+// 	$result = mysqli_query($conn, "SELECT * FROM  users WHERE id = $id");
+// 	$row = mysqli_fetch_assoc($result);
+
+// }else{
+// 	header("Location: login.php");
+// }
+
+
 $isDark = false;
 $isLoggedIn = true;
 $isChecked = false;
