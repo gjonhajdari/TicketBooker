5ea4a5a8e50f42c59a89bd63d41749410a28886a
ZanaMisini <118012775+ZanaMisini@users.noreply.github.com>
2023-05-15 18:09:21 +0200

    sign up fixed

--- src/modules/db.php
+++ src/modules/db.php
@@ -2,9 +2,9 @@
 
 $dbhost = 'localhost:3307';
 $dbbuser = 'root';
-$dbpass = 'password';
-$db = "TicketBooker";
-$conn = mysqli_connect($dbhost, $dbbuser,$dbpass, $db);
+$dbpass = '';
+$db = "ticketbooker";
+$conn = mysqli_connect($dbhost, $dbbuser, $dbpass, $db);
 
 if(! $conn){
 	die('Connection failed: '.mysqli_connect_error());
