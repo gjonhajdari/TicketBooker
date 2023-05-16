a4147995b112441fd2734aff9a2fc1558c4327b3
Kleda <118850687+kledagashi@users.noreply.github.com>
2023-05-16 21:33:29 +0200

    info

--- public/pro.php
+++ public/pro.php
@@ -5,5 +5,5 @@
 
     echo "<h4>Your details!</h4>";
     echo " Hello <b>Name</b> !: " .$name. "<br/><p>We've received your message and we're working on it.</p>" 
-
+    //it's an example for contact.php but it's not working for me, i'll review it later!!
 ?>
