467cd85435714b280bc038ad152223d24a211d37
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-05-17 11:24:49 +0200

    Updated navbar logic

--- src/templates/navbarLoggedin.php
+++ src/templates/navbarLoggedin.php
@@ -1,12 +1,7 @@
-<?php
-	if (session_status() === PHP_SESSION_NONE) {
-		session_start();
-	  }
-?>
 <nav 
 	class="navbar <?php
-						echo $_SESSION["dark_mode"] ? '' : ' navbar-light';
-						echo !$_SESSION["login"] ? '' : ' navbar-logged';
+						echo $isDark ? '' : ' navbar-light';
+						echo !$isDark ? '' : ' navbar-logged';
 					?>">
 	<div class="navbar-content">
 		<a class="navbar-logo" href="index.php">
@@ -18,15 +13,15 @@
 			<a href="contact.php" class="link">Contact</a>
 		</div>
 		<div class="right">
-			<img id="profile-picture" src="assets/images/profiles/profile-picture-<?php echo $_SESSION["avatar"]; ?>.jpg" alt="" width="40" height="40" style="border-radius: 50%;">
-			<p class="name"><?php echo $_SESSION["name"]; ?></p>
+			<img id="profile-picture" src="assets/images/profiles/profile-picture-<?php echo $avatar; ?>.jpg" alt="" width="40" height="40" style="border-radius: 50%;">
+			<p class="name"><?php echo $full_name; ?></p>
 		</div>
 
 		<div class="dropdown"   style= "z-index: 2;">
 			<div class="top">
 				<div class="info">
-					<img src="assets/images/profiles/profile-picture-<?php echo $_SESSION["avatar"]; ?>.jpg" alt="" width="50" height="50" style="border-radius: 50%;">
-					<?php echo $_SESSION["name"]; ?>
+					<img src="assets/images/profiles/profile-picture-<?php echo $avatar; ?>.jpg" alt="" width="50" height="50" style="border-radius: 50%;">
+					<?php echo $full_name; ?>
 				</div>
 		
 				<hr>
@@ -37,7 +32,7 @@
 					<?php echo file_get_contents('assets/icons/profile.svg') ?>
 					<p>Profile</p>
 				</a>
-				<?php if ($_SESSION["user_type"] == 'BUSINESS'): ?>
+				<?php if ($user_type == 'BUSINESS'): ?>
 				<a href="createTicket.php" class="option">
 					<?php echo file_get_contents('assets/icons/create.svg') ?>
 					<p>Create Ticket</p>
