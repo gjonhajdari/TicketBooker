53b07f5887d3a007229523e1f96819f03986d9d6
Vera Llugiqi <vera.llugiqi@outlook.com>
2023-05-20 00:31:10 +0200

    fixed ticketCreate

--- public/createTicket.php
+++ public/createTicket.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include_once('../src/modules/db.php');
 
 if (!isset($_SESSION["login"]) || !$_SESSION["login"]) {
     // Redirect to sign-in page
@@ -13,8 +14,6 @@ $avatar = 10;
 
 
 
-include_once('../src/modules/db.php');
-
 if (isset($_POST['submit']) && $_SESSION["login"] == true) {
     $what = $_POST['what'];
     $location = $_POST['where'];
@@ -22,11 +21,10 @@ if (isset($_POST['submit']) && $_SESSION["login"] == true) {
     $time = $_POST['when_time'];
     $event_title = $_POST['title'];
     $description = $_POST['description'];
-
-    $sql = "INSERT INTO `ticket` (`what`, `location`, `date`, `time`, `event_title`, `description`) 
-            VALUES (?, ?, ?, ?, ?, ?)";
+	$sql = "INSERT INTO ticket (`option`, location, date, time, event_title, description) 
+	VALUES (?, ?, ?, ?, ?, ?)";
     
-    $stm = mysqli_stmt_init($conn);
+	$stm = mysqli_stmt_init($conn);
     if (mysqli_stmt_prepare($stm, $sql)) {
         mysqli_stmt_bind_param($stm, "ssssss", $what, $location, $date, $time, $event_title, $description);
         if (mysqli_stmt_execute($stm)) {
@@ -47,7 +45,7 @@ mysqli_close($conn);
 <html lang="en">
 
 <head>
-	<title>Document</title>
+	<title>Create a Ticket</title>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -75,59 +73,55 @@ mysqli_close($conn);
 
 	<!-- Main content -->
 	<div class="container">
+        <div class="content">
+            <h1 class="header">Create a Ticket</h1>
 
-		<div class="content">
-			<h1 class="header">Create a ticket</h1>
+            <form action="" method="POST" class="inputs">
+                <div class="input-field">
+                    <p class="description">Type and location</p>
+                    <div class="selectors <?php echo $_SESSION["dark_mode"] != "null" ? '' : 'border-light'; ?>">
+                        <select name="what" id="select-what">
+                            <option value="Movie">Movie</option>
+                            <option value="Travel">Travel</option>
+                            <option value="Concert">Concert</option>
+                        </select>
+                        <select name="where" id="select-where">
+                            <option value="prishtine">Prishtinë</option>
+                            <option value="mitrovice">Mitrovicë</option>
+                            <option value="peje">Pejë</option>
+                            <option value="prizren">Prizren</option>
+                            <option value="ferizaj">Ferizaj</option>
+                            <option value="gjilan">Gjilan</option>
+                            <option value="gjakove">Gjakovë</option>
+                        </select>
+                    </div>
+                </div>
 
-			<form action="" class="inputs">
-				<div class="input-field">
-					<p class="description">Type and location</p>
-					<div class="selectors <?php echo $_SESSION["dark_mode"]!="null" ? '' : 'border-light'; ?>">
-						<select name="what" id="select-what">
-							<option value="Movie">Movie</option>
-							<option value="Travel">Travel</option>
-							<option value="Concert">Concert</option>
-						</select>
-						<select name="where" id="select-where">
-							<option value="prishtine">Prishtinë</option>
-							<option value="mitrovice">Mitrovicë</option>
-							<option value="peje">Pejë</option>
-							<option value="prizren">Prizren</option>
-							<option value="ferizaj">Ferizaj</option>
-							<option value="gjilan">Gjilan</option>
-							<option value="gjakove">Gjakovë</option>
-						</select>
-					</div>
-				</div>
+                <div class="input-field">
+                    <p class="description">Date and time</p>
+                    <div class="selectors <?php echo $_SESSION["dark_mode"] != "null" ? '' : 'border-light'; ?>">
+                        <input type="date" name="when_date" id="select-when">
+                        <input type="time" name="when_time" id="select-when">
+                    </div>
+                </div>
 
-				<div class="input-field">
-					<p class="description">Date and time</p>
-					<div class="selectors <?php echo $_SESSION["dark_mode"]!="null" ? '' : 'border-light'; ?>">
-						<input type="date" name="when_date" id="select-when">
-						<input type="time" name="when_time" id="select-when">
-					</div>
-				</div>
+                <div class="input-field <?php echo $_SESSION["dark_mode"] != "null" ? '' : 'border-light'; ?>">
+                    <p class="description">Event title</p>
+                    <input type="text" name="title" placeholder="Title">
+                </div>
 
-				<div class="input-field <?php echo $_SESSION["dark_mode"]!="null" ? '' : 'border-light'; ?>">
-					<p class="description">Event title</p>
-					<input type="text" name="title" placeholder="Title">
-				</div>
+                <div class="input-field <?php echo $_SESSION["dark_mode"] != "null" ? '' : 'border-light'; ?>">
+                    <p class="description">Event description</p>
+                    <textarea name="description" cols="30" rows="12" placeholder="Description"></textarea>
+                </div>
 
-				<div class="input-field <?php echo $_SESSION["dark_mode"]!="null" ? '' : 'border-light'; ?>">
-					<p class="description">Event description</p>
-					<textarea name="description" cols="30" rows="12" placeholder="Description"></textarea>
-				</div>
+                <input type="submit" id="button" name="submit" value="Create ticket">
+            </form>
+        </div>
+    </div>
 
-				<input type="submit" id="button" name="submit" value="Create ticket">
-
-			</form>
-
-		</div>
-
-	</div>
-
-	<!-- Footer -->
-	<?php include "../src/templates/footer.php" ?>
+    <!-- Footer -->
+    <?php include "../src/templates/footer.php" ?>
 </body>
 
 </html>
