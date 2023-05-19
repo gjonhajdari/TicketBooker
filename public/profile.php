0e7bafbd50ea8224784de414df52e1dd57a1993d
Kleda <118850687+kledagashi@users.noreply.github.com>
2023-05-19 23:21:39 +0200

    linked profile.php with index-ajax.php

--- public/profile.php
+++ public/profile.php
@@ -24,6 +24,8 @@ $title = 'Dua Lipa';
 ?>
 
 
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -58,6 +60,7 @@ $title = 'Dua Lipa';
 
 	<main class="container">
 
+	<div class="topandbottom">
 		<div class="top">
 
 			<h1>Welcome back, <?php echo $_SESSION["name"]; ?></h1>
@@ -67,18 +70,35 @@ $title = 'Dua Lipa';
 			<div class='bottom'>
 			
 		
-			 <button class="btn <?php echo (isset($_SESSION["login"]) && $_SESSION["login"]) ? 
+			 <input type="submit" id="submit" onclick="funct()" class="btn  <?php echo (isset($_SESSION["login"]) && $_SESSION["login"]) ? 
 			 					(isset($_SESSION["dark_mode"]) && ($_SESSION["dark_mode"] == "null") ? 
-								'' : 'btn-dark') : 'btn-dark'; ?>">
-					
-				Your tickets
+								'' : 'btn-dark') : 'btn-dark'; ?>" onclick="check">
 				<?php echo file_get_contents('assets/icons/arrow.svg') ?>
-			</button>
+				
+			</input>
+
+			<?php 
+			//require_once('../src/modules/db.php');
+			//if(($_SESSION['user_type']== "BUSINESS")){
+			//	 include "link href='./public/ajax/index-ajax.php'>";
+				 
+		    //	} else {
+			//	echo "Cannot access database!";
+		     //	}	?>
+
+			<script>
+				function funct() { 
+					location.replace("/TicketBooker/public/ajax/index-ajax.php");
+				 }
+	
+			</script>	
+
+			
 
 			</div>
 			
 		</div>
-
+	</div>
 
 
 		<div class="tabs row g-4">
