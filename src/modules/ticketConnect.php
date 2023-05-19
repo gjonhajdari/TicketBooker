2f90a6be60c0bfb78d5684b7ff569f58b9288a50
Vera Llugiqi <vera.llugiqi@outlook.com>
2023-05-20 01:33:08 +0200

    divided the php code for signup and create ticket

--- src/modules/ticketConnect.php
+++ src/modules/ticketConnect.php
@@ -1,4 +1,19 @@
-<style>	#button:hover, #button1:hover, #button2:hover {
+<style>
+		#button, #button1, #button2 {
+		padding: 1rem 2rem;
+		font-size: 1.25rem;
+		border: none;
+		border-radius: 1rem;
+		width: 100%;
+		background-color: var(--accent);
+		font-weight: 600;
+		cursor: pointer;
+		transition: 300ms;
+		color: var(--background);
+		margin-top: 30px;
+		}
+
+		#button:hover, #button1:hover, #button2:hover {
 		transform: translateY(-5px);
 		}
 		.alert {
@@ -22,24 +37,10 @@
  		border-color: #f5c6cb;
 		}
 	</style>
-		#button, #button1, #button2 {
-		padding: 1rem 2rem;
-		font-size: 1.25rem;
-		border: none;
-		border-radius: 1rem;
-		width: 100%;
-		background-color: var(--accent);
-		font-weight: 600;
-		cursor: pointer;
-		transition: 300ms;
-		color: var(--background);
-		margin-top: 30px;
-		}
 
-	
-
-<?php 
+<?php
 require_once("db.php");
+
 if (isset($_POST['submit']) && $_SESSION["login"] == true) {
     $what = $_POST['what'];
     $location = $_POST['where'];
@@ -48,29 +49,37 @@ if (isset($_POST['submit']) && $_SESSION["login"] == true) {
     $event_title = $_POST['title'];
     $description = $_POST['description'];
 
-	$currentDate = date('Y-m-d');
-    if ($date < $currentDate) {
-        echo "<div class='alert alert-danger w-50 p-3'>Invalid date. Please select a current or future date.</div>";
+    // Check if any of the fields are empty
+    if (empty($what) || empty($location) || empty($date) || empty($time) || empty($event_title) || empty($description)) {
+        echo "<div class='alert alert-danger w-50 p-3'>Please fill in all the required fields.</div>";
     } else {
-	$sql = "INSERT INTO ticket (`option`, location, date, time, event_title, description) 
-	VALUES (?, ?, ?, ?, ?, ?)";
- 
-	$stm = mysqli_stmt_init($conn);
-    if (mysqli_stmt_prepare($stm, $sql)) {
-        mysqli_stmt_bind_param($stm, "ssssss", $what, $location, $date, $time, $event_title, $description);
-        if (mysqli_stmt_execute($stm)) {
-            mysqli_stmt_close($stm);
-            mysqli_close($conn);
-            echo '<script>window.location.href = "../../../TicketBooker/public/createTicket.php";</script>';
-            exit;
-        } else {
-            die("Error executing the statement: " . mysqli_error($conn));
-        }
-    } else {
-        die("Error preparing the statement: " . mysqli_error($conn));
-    }
-  }
-}
-mysqli_close($conn);
+        $currentDate = date('Y-m-d');
+        $currentTime = date('H:i');
+        $selectedDateTime = $date . ' ' . $time;
 
+        if ($date < $currentDate || ($date == $currentDate && $time < $currentTime)) {
+            echo "<div class='alert alert-danger w-50 p-3'>Invalid date or time. Please select a future date and time.</div>";
+        } else {
+            $sql = "INSERT INTO ticket (`option`, location, date, time, event_title, description) 
+            VALUES (?, ?, ?, ?, ?, ?)";
+         
+            $stm = mysqli_stmt_init($conn);
+            if (mysqli_stmt_prepare($stm, $sql)) {
+                mysqli_stmt_bind_param($stm, "ssssss", $what, $location, $date, $time, $event_title, $description);
+                if (mysqli_stmt_execute($stm)) {
+                    mysqli_stmt_close($stm);
+                    mysqli_close($conn);
+                    echo '<script>window.location.href = "../../../TicketBooker/public/createTicket.php";</script>';
+                    exit;
+                } else {
+                    die("Error executing the statement: " . mysqli_error($conn));
+                }
+            } else {
+                die("Error preparing the statement: " . mysqli_error($conn));
+            }
+        }
+    }
+}
+
+mysqli_close($conn);
 ?>
