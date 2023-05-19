2ac8c01c4c91fb8e5207da352270d59714f0d77b
Vera Llugiqi <vera.llugiqi@outlook.com>
2023-05-19 10:45:41 +0200

    fixed errors in index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
-<?php 
-	session_start();
-	$isDark = true;
-	$isLoggedIn = true;
-	$avatar = 10;
-	$full_name = 'Gjon Hajdari';
-
+<?php
+session_start();
+$isDark = true;
+$isLoggedIn = true;
+$avatar = 10;
+$full_name = 'Gjon Hajdari';
 ?>
 
 <!DOCTYPE html>
@@ -17,81 +16,72 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel='icon' type='image/x-icon' href='assets/icons/favicon.svg'>
 	<?php
-		if (($_SESSION["login"]==true) && ($_SESSION['dark_mode'] != "null")) {
-			echo "<link rel='stylesheet' href='css/palette-dark.css'>";
-		} else if(($_SESSION["login"]==true) && ($_SESSION['dark_mode'] == "null")){
-			echo "<link rel='stylesheet' href='css/palette-light.css'>";
-		}else{
-			echo "<link rel='stylesheet' href='css/palette-dark.css'>";
-		}
+	if (isset($_SESSION["login"]) && ($_SESSION["login"] == true) && isset($_SESSION['dark_mode']) && ($_SESSION['dark_mode'] != "null")) {
+		echo "<link rel='stylesheet' href='css/palette-dark.css'>";
+	} else if (isset($_SESSION["login"]) && ($_SESSION["login"] == true) && isset($_SESSION['dark_mode']) && ($_SESSION['dark_mode'] == "null")) {
+		echo "<link rel='stylesheet' href='css/palette-light.css'>";
+	} else {
+		echo "<link rel='stylesheet' href='css/palette-dark.css'>";
+	}
 	?>
 	<link rel="stylesheet" href="css/general.css">
 	<link rel="stylesheet" href="css/index.css">
 	<script src="https://kit.fontawesome.com/26e97bbe8d.js" crossorigin="anonymous"></script>
-	<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU="
-	crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 	<script src="js/app.js"></script>
 </head>
 
 <body class="<?php echo $isDark ? '' : 'body-light'; ?>">
-<form action="../src/modules/ticketfinder.php" method="POST" enctype="multipart/form-data">
-	<!-- Navigation Bar -->
-	<?php ($_SESSION["login"]==true) ?  include "../src/templates/navbarLoggedin.php" : include "../src/templates/navbar.php"   ?>
+	<form action="../src/modules/ticketfinder.php" method="POST" enctype="multipart/form-data">
+		<!-- Navigation Bar -->
+		<?php (isset($_SESSION["login"]) && $_SESSION["login"] == true)  ?  include "../src/templates/navbarLoggedin.php" : include "../src/templates/navbar.php";   ?>
 
-	<!-- Main content -->
-	<main>
-		
-		<div class="content">
-			<h1 id="title">
-				Book tickets
-				<br>anywhere, all in <span id="accent">one place</span>
-			</h1>
+		<!-- Main content -->
+		<main>
+			<div class="content">
+				<h1 id="title">
+					Book tickets
+					<br>anywhere, all in <span id="accent">one place</span>
+				</h1>
 
-			<div class="options">
-				<p>What, when, where?</p>
+				<div class="options">
+					<p>What, when, where?</p>
 
-				<div class="selectors <?php 
-					if($_SESSION['login'])
-					echo $_SESSION["dark_mode"]!="null" ? '' : 'border-light'; 
-					else
-					echo '';
-				?>">
-					<select name="type" id="select-what">
-						<option value="" disabled selected>Type</option>
-						<option value="Movie">Movie</option>
-						<option value="Travel">Travel</option>
-						<option value="Concert">Concert</option>
-					</select>
-					<input type="date" name="when" id="select-when">
-					<select name="location" id="select-where">
-						<option value="" disabled selected>Location</option>
-						<option value="prishtine">Prishtinë</option>
-						<option value="mitrovice">Mitrovicë</option>
-						<option value="peje">Pejë</option>
-						<option value="prizren">Prizren</option>
-						<option value="ferizaj">Ferizaj</option>
-						<option value="gjilan">Gjilan</option>
-						<option value="gjakove">Gjakovë</option>
-					</select>
+			  		<div class="selectors <?php if (isset($_SESSION['login']) &&   $_SESSION['login']) 
+			  				                   echo ($_SESSION["dark_mode"] != "null") ? '' : 'border-light'; 
+											   else echo ''; ?>">
+
+						<select name="type" id="select-what">
+							<option value="" disabled selected>Type</option>
+							<option value="Movie">Movie</option>
+							<option value="Travel">Travel</option>
+							<option value="Concert">Concert</option>
+						</select>
+						<input type="date" name="when" id="select-when">
+						<select name="location" id="select-where">
+							<option value="" disabled selected>Location</option>
+							<option value="prishtine">Prishtinë</option>
+							<option value="mitrovice">Mitrovicë</option>
+							<option value="peje">Pejë</option>
+							<option value="prizren">Prizren</option>
+							<option value="ferizaj">Ferizaj</option>
+							<option value="gjilan">Gjilan</option>
+							<option value="gjakove">Gjakovë</option>
+						</select>
+					</div>
 				</div>
+
+				<button class="btn <?php echo (isset($_SESSION["login"]) && $_SESSION["login"]) ? ($_SESSION["dark_mode"] == "null" ? '' : 'btn-dark') : 'btn-dark'; ?>">
+					Find tickets
+					<?php echo file_get_contents('assets/icons/arrow.svg'); ?>
+				</button>
 			</div>
+			<img src="assets/images/tickets.png" alt="Tickets" id="tickets" height="500">
+		</main>
 
-			<button class="btn"  <?php
-			if($_SESSION["login"])
-			 echo $_SESSION["dark_mode"]=="null" ? '' : 'btn-dark';
-			 else
-			 echo 'btn-dark'; 
-			 ?> >
-				Find tickets
-				<?php echo file_get_contents('assets/icons/arrow.svg') ?>
-			</button>
-		</div>
-		<img src="assets/images/tickets.png" alt="Tickets" id="tickets" height="500">
-	</main>
-
-	<!-- Footer -->
-	<?php include('../src/templates/footer.php') ?>
-
+		<!-- Footer -->
+		<?php include('../src/templates/footer.php'); ?>
+	</form>
 </body>
 
 </html>
