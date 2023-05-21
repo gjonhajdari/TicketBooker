f435f4f196889c09e540b37a0e93882a7fc16ebb
KlajdiGashi <klajdi.gashi@student.uni-pr.edu>
2023-05-21 19:22:55 +0200

    deleteticket code

--- src/modules/deleteticket.php
+++ src/modules/deleteticket.php
@@ -1,15 +1,26 @@
 <?php
 
 // deleteticket.php
-
-
 include_once('db.php');
+session_start();
 
-$ttid = $_POST['ttid'] ?? '';
-$tid = $_POST['tid'] ?? '';
+ if ($_SESSION['id']) {
+    $id = $_SESSION['id'];
+    $sql = "SELECT * FROM `user` WHERE id = '$id'";
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $user_type = $user['user_type'];
+    $isLoggedIn = true;
+} else {
+    $isDark = true;
+    $isLoggedIn = false;
+}
 
+
+$tid=$_SESSION["tid"];
+$id=$_SESSION['id'];
 // Perform the database deletion
-$deleteQuery = "DELETE FROM user_ticket WHERE ticket_id = '$ttid' AND user_id = '$tid'";
+$deleteQuery = "DELETE FROM user_ticket WHERE ticket_id = '$tid' AND user_id = '$id'";
 
 $deleteResult = mysqli_query($conn, $deleteQuery);
 
